6e17d5924280f02bff2d2191ea528dd0182b1f02
Rakim02 <Rakim1302@gmail.com>
2025-10-02 16:17:11 +0100

    Update backend.tf

--- EKSCluster/backend.tf
+++ EKSCluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "rakimsterraformremote" # CHANGE WITH YOUR S3 BUCKET
-    key    = "K8inEKS/terraform.tfstate"
+    key    = "k8inEKS/terraform.tfstate"
     region = "eu-west-2"
   }
 }
